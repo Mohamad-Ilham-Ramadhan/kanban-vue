77f36a49c1bdccff5845951ec60f058d0c90ce1f
Mohamad-Ilham-Ramadhan <mohamadilhamramadhan@gmail.com>
2023-11-14 23:24:05 +0700

    styling: layout done

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,49 @@ import Logo from '@/assets/logo.svg'
 
 const store = useBoardStore()
 console.log('store', store)
+
+  function toggleTheme() {
+    document.documentElement.classList.toggle('dark')
+  }
 </script>
 
 <template>
-  <header class="flex flex-row h-[96px] bg-white dark:bg-dark-light">
-    <div class="shrink-0 w-[300px]"></div>
+  <header class="flex flex-row items-center w-full h-[96px] fixed left-0 top-0 z-50 bg-white dark:bg-dark-light border-b border-b-slate-700">
+    <div class="shrink-0 w-[300px] h-full flex items-center border-r border-r-slate-700 pl-8">
+      <img src="@/assets/logo.svg" alt="logo">
+    </div>
+    <div class="flex items-center px-8">
+      <div class="font-bold text-2xl">Platform Launch</div>
+    </div>
   </header>
+  <div class="flex flex-row">
+    <aside class="shrink-0 w-[300px] h-[100vh] fixed left-0 top-0 z-40 dark:bg-dark-light bg-white border-r border-r-slate-700 pt-[96px]">
+      <div class="h-full pl-8 pr-6 beautify-scrollbar overflow-auto">
+        <button @click="toggleTheme()">Toggle theme</button>
+  
+        <div class="overflow-auto beautify-scrollbar">
+          <div class="bg-red-300 h-[1200px] flex flex-col justify-between ">
+            <div>item 1</div>
+            <div>item 2</div>
+            <div>item 3</div>
+          </div>
+        </div>
+      </div>
+    </aside>
+    <main class="pl-[300px] pt-[96px] pb-[40px] flex w-full h-[100vh] overflow-hidden">
+      <div class="beautify-scrollbar w-[100vw] h-[calc(100vh-96px)] overflow-auto">
+        <div class="flex w-full h-full">
+          <div class="shrink-0 w-[200px] bg-dark-light mr-4">item 1</div>
+          <div class="shrink-0 w-[200px] bg-dark-light mr-4">item 1</div>
+          <div class="shrink-0 w-[200px] bg-dark-light mr-4">item 1</div>
+          <div class="shrink-0 w-[200px] h-[1200px] bg-dark-light mr-4">item 1</div>
+          <div class="shrink-0 w-[200px] bg-dark-light mr-4">item 1</div>
+          <div class="shrink-0 w-[200px] bg-dark-light mr-4">item 1</div>
+          <div class="shrink-0 w-[200px] bg-dark-light mr-4">item 8</div>
+        </div>
+      </div>
+    </main>
+  </div>
 </template>
 
 <style scoped></style>
