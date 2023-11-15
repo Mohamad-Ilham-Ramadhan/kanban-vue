628e9ec9e6f11061ccfb1da77abdf50d510e0fd3
Mohamad-Ilham-Ramadhan <mohamadilhamramadhan@gmail.com>
2023-11-16 05:14:53 +0700

    style: styling Modal component for appropriate light-theme

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,28 +1,28 @@
 <script setup >
-   import { ref, Teleport, Transition, defineProps, onMounted, onUpdated } from 'vue'
-   const props = defineProps({open: false})
-   const modalRef = ref(null)
-   onUpdated(() => {
-      console.log(props.open, modalRef.value)
-   })
+import { ref, Teleport, Transition, defineProps, onUpdated } from 'vue'
+const props = defineProps(['open', 'cssClass'])
+const modalRef = ref(null)
+onUpdated(() => {
+  console.log(props.open, modalRef.value)
+})
 </script>
 
 <template>
-  <div>
-    <!-- modal -->
-    <Transition name="modal">
-      <div v-if="props.open" ref="modalRef"
-        class="p-8 text-white rounded-lg bg-slate-800 fixed z-[1000] top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2">
-        <slot></slot>
-      </div>
-    </Transition>
-    <!-- overlay -->
-    <Teleport to="body">
-      <Transition>
-        <div v-if="props.open" @click="$emit('close-modal')"  class="bg-transparent fixed inset-0 z-[100]"></div>
+  <Teleport to="body">
+    <div>
+      <!-- modal -->
+      <Transition name="modal">
+        <div v-if="props.open" ref="modalRef"
+          :class="['p-8 text-white rounded-lg bg-white text-black dark:bg-dark-light fixed z-[1000] top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2', cssClass]">
+          <slot></slot>
+        </div>
       </Transition>
-    </Teleport>
-  </div>
+      <!-- overlay -->
+      <Transition>
+        <div v-if="props.open" @click="$emit('close-modal')" class="bg-dark opacity-50 fixed inset-0 z-[100]"></div>
+      </Transition>
+    </div>
+  </Teleport>
 </template>
 
 <style scoped>
@@ -34,5 +34,6 @@
 
 .modal-enter-from,
 .modal-leave-to {
-   opacity: 0;
-}</style>
+  opacity: 0;
+}
+</style>
