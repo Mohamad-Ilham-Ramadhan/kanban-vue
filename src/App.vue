f2d54394c120dfd99d72d48dfd6fd021340d32f6
Mohamad-Ilham-Ramadhan <mohamadilhamramadhan@gmail.com>
2023-11-15 13:11:24 +0700

    styling: nav

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <script setup>
 import { ref, Teleport, Transition, watch } from 'vue'
-import { useBoardStore } from '@/stores/board';
 import Logo from '@/components/Logo.vue'
 import Button from './components/Button.vue';
 import IconEllipsis from '@/components/icons/IconEllipsis.vue'
-
-const store = useBoardStore()
-console.log('store', store)
+import IconBoard from '@/components/icons/IconBoard.vue'
+// store
+import { useBoardStore } from '@/stores/board.js'
+const boardStore = useBoardStore()
+const boards = boardStore.boards
+const board = boardStore.boards[boardStore.activeBoard]
+console.log('board', board)
 
   function toggleTheme() {
     document.documentElement.classList.toggle('dark')
@@ -21,7 +24,7 @@ console.log('store', store)
       <Logo />
     </div>
     <div class="flex flex-row w-full justify-between items-center px-8">
-      <div class="font-bold text-2xl">Platform Launch</div>
+      <div class="font-bold text-2xl">{{ board.name }}</div>
       <div class="flex items-center">
         <Button text="+ Add New Task" class="mr-4" />
         <button class="block p-2.5 rounded-xl text-slate-400 hover:bg-slate-200 dark:hover:bg-dark transition-colors">
@@ -32,12 +35,18 @@ console.log('store', store)
   </header>
   <div class="flex flex-row">
     <aside class="shrink-0 w-[300px] h-[100vh] fixed left-0 top-0 z-40 dark:bg-dark-light bg-white border-r border-r-slate-700 pt-[96px]">
-      <div class="h-full pl-8 pr-6 beautify-scrollbar overflow-auto">
-        <button @click="toggleTheme()">Toggle theme</button>
-  
-        <div class="flex flex-col justify-between ">
-          <!--  -->
+      <div class="h-full pt-4 beautify-scrollbar overflow-auto">
+        <div class="uppercase text-[.7rem] text-slate-500 dark:text-slate-400 font-bold tracking-[.175rem] pl-8 mb-4">
+          all boards ({{ boardStore.boards.length }})
         </div>
+        <nav class="flex flex-col justify-between pr-6 mb-2">
+          <li v-for="b in boards" class="list-none font-bold flex items-center dark:text-slate-400 text-slate-500 hover:bg-primary hover:text-white hover:cursor-pointer pl-8 py-3 rounded-r-full mb-1">
+            <IconBoard class="mr-4" />
+            <div>{{ b.name }}</div>
+          </li>
+        </nav>
+        <Button @click="toggleTheme()" text="Toggle theme" />
+  
       </div>
     </aside>
     <main class="pl-[300px] pt-[96px] pb-[40px] flex w-full h-[100vh] overflow-hidden">
