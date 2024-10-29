d2fe569244722b8f05f2fa9d052caa114fceeaaf
Mohamad-Ilham-Ramadhan <mohamadilhamramadhan@gmail.com>
2024-10-29 09:46:05 +0700

    fix build error

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [
     vue(),
-    'optionalChainingAssign',
+    // 'optionalChainingAssign',
   ],
   resolve: {
     alias: {
